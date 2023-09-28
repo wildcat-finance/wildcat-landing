cde7f72cc1012d0c8435efdf3d3babc9ab47791d
sha237 <sha237@proton.me>
2023-09-28 14:22:11 +0200

    feat: do most of templating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,240 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='flex min-h-screen flex-col'>
+      <nav className='h-20 flex flex-row justify-between items-center bg-[#232323] px-10'>
+        <div>
+          <Image src='/logo.png' alt='logo' width={150} height={51} />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div className='flex flex-row items-center gap-6'>
+          <a href='#' className='text-white'>
+            Docs
+          </a>
+          <button className='rounded-xl bg-white px-4 py-2 text-black min-h-6 text-sm'>
+            Launch app
+          </button>
+        </div>
+      </nav>
+      <section className='h-[calc(100vh-80px)] flex flex-col items-center justify-center gap-8 px-16'>
+        <video
+          className='absolute inset-x-0 top-20 bottom-0 object-cover'
+          autoPlay
+          muted
+          loop
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+          <source
+            src='/WILDCAT_TOP_BANNER_480.mp4'
+            type='video/mp4'
+            media='(max-width: 480px)'
+          />
+          <source
+            src='/WILDCAT_TOP_BANNER_480.webm'
+            type='video/webm'
+            media='(max-width: 480px)'
+          />
+          <source
+            src='/WILDCAT_TOP_BANNER_720.mp4'
+            type='video/mp4'
+            media='(max-width: 720px)'
+          />
+          <source
+            src='/WILDCAT_TOP_BANNER_720.webm'
+            type='video/webm'
+            media='(max-width: 720px)'
+          />
+          <source src='/WILDCAT_TOP_BANNER_1080.mp4' type='video/mp4' />
+          <source src='/WILDCAT_TOP_BANNER_1080.webm' type='video/webm' />
+        </video>
+        <div className='z-10 flex flex-col gap-8'>
+          <h1 className='text-5xl/[50px] text-center'>
+            Unlocking flexible fixed-rate credit, on-chain.
+          </h1>
+          <p className='text-center'>
+            Wildcat is a new DeFi lending protocol that empowers borrowsers to
+            set their own terms while offering the flexibility to borrow any
+            ERC20 token they require.
           </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <div className='flex flex-row gap-4 items-center justify-center'>
+            <button className='rounded-xl bg-white px-4 py-2 text-black min-h-6 text-sm'>
+              Become a Borrower
+            </button>
+            <button className='rounded-xl bg-white px-4 py-2 text-black min-h-6 text-sm'>
+              Launch app
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className='bg-white text-black'>
+        <h2 className='text-2xl py-20 text-center font-black'>How it works</h2>
+        <div className='grid grid-cols-12 gap-3 px-8 pb-16 max-w-7xl mx-auto'>
+          <div className='col-span-3'>
+            <h3 className='text-2xl border-y border-[#c8c7bf] mb-4 font-black'>
+              1
+            </h3>
+            <h4 className='font-extrabold'>Create your vault</h4>
+            <p>
+              Whitelisted borrowers can create custom vaults for any ERC20 token
+              they desire.
+            </p>
+          </div>
+          <div className='col-span-3'>
+            <h3 className='text-2xl border-y border-[#c8c7bf] mb-4 font-black'>
+              2
+            </h3>
+            <h4 className='font-extrabold'>Choose your terms</h4>
+            <p>
+              Specify your vault’s terms at deployment, including APR, vault
+              capacity, reserve ratio, withdrawal cycles, and more.
+            </p>
+          </div>
+          <div className='col-span-3'>
+            <h3 className='text-2xl border-y border-[#c8c7bf] mb-4 font-black'>
+              3
+            </h3>
+            <h4 className='font-extrabold'>Lend and earn securely</h4>
+            <p>
+              Borrowers can whitelist lenders to interact with their vaults,
+              allowing them to lend on-chain.
+            </p>
+          </div>
+          <div className='col-span-3'>
+            <h3 className='text-2xl border-y border-[#c8c7bf] mb-4 font-black'>
+              4
+            </h3>
+            <h4 className='font-extrabold'>Adjust your Vault’s conditions </h4>
+            <p>
+              Borrowers can change open vault’s APRs based on their needs,
+              automatically impacting the reserve ratio.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section
+        className='flex flex-col items-center justify-center bg-cover bg-no-repeat'
+        style={{ backgroundImage: "url(/pink-bg.jpeg)" }}
+      >
+        <h2 className='text-5xl text-black text-center py-6 pt-20'>
+          Interested in Wildcat?
+        </h2>
+        <span className='text-black text-center py-6'>Apply as</span>
+        <div className='flex flex-row gap-6 items-center justify-center pb-20'>
+          <button className='bg-black rounded-full w-[158px] py-2'>
+            Borrower
+          </button>
+          <button className='bg-black rounded-full w-[158px] py-2'>
+            Lender
+          </button>
+        </div>
+      </section>
+      <section className='bg-white text-black py-12'>
+        <div className='max-w-7xl mx-auto'>
+          <h2 className='text-5xl text-black text-center pb-12'>
+            Wildcat banking: a historical tool for a modern age
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+          <div className='grid grid-cols-12 gap-8'>
+            <div className='col-span-4'>
+              <Image
+                className='mb-6'
+                src='/wildcat-symbol.png'
+                alt='logo'
+                width={50}
+                height={80}
+              />
+              <h3>Flexible Borrowing for Liquidity Providers</h3>
+              <p>
+                Empower liquidity provider to access inventory on-chain, with
+                minimal back-office coordination needs.
+              </p>
+            </div>
+            <div className='col-span-4'>
+              <Image
+                className='mb-6'
+                src='/wildcat-symbol.png'
+                alt='logo'
+                width={50}
+                height={80}
+              />
+              <h3>Flexible Borrowing for Liquidity Providers</h3>
+              <p>
+                Empower liquidity provider to access inventory on-chain, with
+                minimal back-office coordination needs.
+              </p>
+            </div>
+            <div className='col-span-4'>
+              <Image
+                className='mb-6'
+                src='/wildcat-symbol.png'
+                alt='logo'
+                width={50}
+                height={80}
+              />
+              <h3>Flexible Borrowing for Liquidity Providers</h3>
+              <p>
+                Empower liquidity provider to access inventory on-chain, with
+                minimal back-office coordination needs.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='bg-[#E9E8DD] text-black'>
+        <h2 className='text-center text-2xl py-20'>
+          Transparency as a core value
+        </h2>
+        <div className='grid grid-cols-12 gap-4 max-w-3xl mx-auto h-[calc(315px+40px)] pb-10'>
+          <div className='col-span-6 rounded-xl bg-white p-6 h-full'>
+            <p>
+              Borrowers and Lenders can visualize each Vault’s transaction log,
+              providing a transparent and comprehensive record of all activities
+              and interactions.
+            </p>
+          </div>
+          <div className='col-span-6 rounded-xl bg-white p-6 h-full flex flex-col items-center justify-between'>
+            <span>Audited by</span>
+            <Image src='/code4rena.png' alt='logo' width={200} height={39} />
+            <button className='bg-black rounded-full w-[158px] py-2 text-white'>
+              Read Reports
+            </button>
+          </div>
+        </div>
+      </section>
+      <section
+        className='flex flex-col items-center justify-center bg-cover bg-no-repeat'
+        style={{ backgroundImage: "url(/flowers-bg.jpeg)" }}
+      >
+        <div className='max-w-7xl mx-auto pt-12 pb-20'>
+          <h3 className='font-black text-2xl'>Manifesto</h3>
+          <h2 className='font-extrabold text-5xl py-12'>
+            Wildcat addresses the gaps of existing lending protocols
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <div className='grid grid-cols-12 gap-8'>
+            <div className='col-span-6'>
+              <p className='mb-8'>
+                Freeing borrowers to choose their desired underlying asset,
+                interest rate methodology, and without the use of middlemen.
+              </p>
+              <button className='bg-white rounded-full w-[158px] py-2 text-black text-sm'>
+                Read our Manifesto
+              </button>
+            </div>
+            <div className='col-span-6'>
+              <p>
+                Wildcat is primarily aimed at organizations wishing to borrow
+                funds in the medium-term, but can reasonably be extended to
+                parties such as DeFi protocols wishing to raise funds without
+                the consequences of selling a native-token filled treasury into
+                the ground to do so. As a protocol that is - in its current form
+                - reliant upon counterparty selection by borrowers, Wildcat is
+                fundamentally permissioned in nature.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
